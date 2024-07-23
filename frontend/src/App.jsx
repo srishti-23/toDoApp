c83924797caa8a54ec72c0869f2b4e47cb0c39dc
srishti-23 <142992320+srishti-23@users.noreply.github.com>
2024-07-23 14:18:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,50 +29,42 @@ const App = () => {
 
   return (
     <>
-        <div
+      <div
+        className="w-full h-screen flex justify-center items-center bg-cover bg-no-repeat bg-center"
         style={{
           backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: "cover", // Adjust as needed
-          backgroundRepeat:'no-repeat',
-          backgroundPosition: "center", // Adjust as needed
-          /* Additional styling if needed */
-          width: "100%",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
         }}
       >
-      <h1 className="text-3xl font-bold mt-[-460px] ml-12 ">
-        ToDo App
-      </h1>
-      <div className=" fixed w-1/2 my-2 p-4  h-80 ml-20">
-        <div className="flex flex-row">
-          <input
-            type="text"
-            placeholder="Write Todos here.."
-            className="p-2 ml-2 w-full	rounded-md"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-          <button
-            className="bg-green-500 ml-2 text-white rounded-md p-2"
-            onClick={handleAddOrUpdate}
-          >
-            {updatingTodo ? "UPDATE" : "ADD"}
-          </button>
+        <div className="text-center md:mt-[-460px] md:ml-12">
+          <h1 className="text-3xl font-bold mb-4 md:mb-0">ToDo App</h1>
         </div>
-        <div className="list-none rounded-md p-2 mt-2 flex flex-col text-white">
-          {todo.map((item) => (
-            <ToDO
-              key={item._id}
-              text={item.text}
-              updateToDo={() => updateMode(item._id, item.text)}
-              deleteToDo={() => deleteToDo(item._id, setTodo)}
+        <div className="fixed w-4/5 md:w-1/2 my-2 p-4 h-80 ml-5 md:ml-20 bg-white bg-opacity-70 rounded-md shadow-lg">
+          <div className="flex flex-col md:flex-row mb-4">
+            <input
+              type="text"
+              placeholder="Write Todos here.."
+              className="p-2 mb-2 md:mb-0 md:ml-2 w-full rounded-md"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
             />
-          ))}
+            <button
+              className="bg-green-500 text-white rounded-md p-2 md:ml-2"
+              onClick={handleAddOrUpdate}
+            >
+              {updatingTodo ? "UPDATE" : "ADD"}
+            </button>
+          </div>
+          <div className="list-none rounded-md p-2 mt-2 flex flex-col text-gray-800">
+            {todo.map((item) => (
+              <ToDO
+                key={item._id}
+                text={item.text}
+                updateToDo={() => updateMode(item._id, item.text)}
+                deleteToDo={() => deleteToDo(item._id, setTodo)}
+              />
+            ))}
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
